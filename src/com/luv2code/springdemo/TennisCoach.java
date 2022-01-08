7202fb273dacbfc95215655b7471bcbcf1f4e735
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-01-08 16:28:11 -0300

    Using Default Bean ID

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
