026505e88c608635a284eb8d16bb364b59abbaab
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-01-17 10:20:03 -0300

    Testing bean scope options - singleton (default) and prototype

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 	private FortuneService fortuneService;
 
