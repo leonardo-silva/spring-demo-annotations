18499f47c4d642c324f5edb3d7910a63a5e987a0
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-01-17 10:39:07 -0300

    Using @PostConstruct and @PreDestroy with singleton scope

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+@Scope("singleton")
 public class TennisCoach implements Coach {
 	private FortuneService fortuneService;
 
@@ -16,6 +19,18 @@ public class TennisCoach implements Coach {
 		System.out.println("Inside TennisCoach constructor...");
 	}
 
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
+	}
+		
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
