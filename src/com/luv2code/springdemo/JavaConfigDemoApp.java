ae4de3cbadf8554b1c1fea6000530c38184098ec
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-01-20 20:47:00 -0300

    Spring Configuration with Java Code (no xml)

---
+++ src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		// get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// call a method on the bean
+		System.out.println("JavaConfigDemoApp >> " + theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		// close the context
+		context.close();
+	}
+
+}
