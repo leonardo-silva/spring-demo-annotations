f7e3d6b399b558b69dce7da9a43154f38e6dfee8
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-01-25 14:25:44 -0300

    Defining Beans with Java code (no xml)

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,10 +1,24 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
+// The line above is not necessary (no component scan) is the beans are configured manually, 
+// as in the code below.
 public class SportConfig {
 
+	// define bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our swim coach AND inject dependency
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
 }
---
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo;
+
+public class SwimCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Swim 1000 meters as a warm up.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return this.fortuneService.getFortune();
+	}
+
+}
